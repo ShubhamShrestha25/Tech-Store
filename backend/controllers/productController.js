0010355b52e98f8e514a356e587a698583aff1e0
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-10-24 20:52:27 +0545

    product routes done

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require("../models/productModel");
 
-// Create Product
+// Create Product -- Admin
 exports.createProduct = async (req, res, next) => {
   const product = await Product.create(req.body);
   res.status(201).json({
@@ -9,6 +9,71 @@ exports.createProduct = async (req, res, next) => {
   });
 };
 
-exports.getAllProducts = (req, res) => {
-  res.status(200).json("Route is wroking fine");
+// Get All Products
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+  res.status(200).json({
+    success: true,
+    products,
+  });
+};
+
+// Get Product Details
+exports.getProductDetails = async (req,res,next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product Not Found",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+
+}
+
+// Update Product -- Admin
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product Not Found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+// Delete product
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product Not Found",
+    });
+  }
+
+  await product.remove();
+
+  res.status(200).json({
+    success: true,
+    message: "Product Delete Successfully",
+  });
 };
