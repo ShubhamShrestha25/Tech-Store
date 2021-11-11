0511d1af3509ee904ff592f5dc3940499349da86
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-11-11 19:22:15 +0545

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
-import Header from "./component/layout/Header.js";
+import Header from "./component/layout/header/Header.js";
 import { BrowserRouter as Router } from "react-router-dom";
 import webFont from "webfontloader";
 import React, { useEffect } from "react";
+import Footer from "./component/layout/footer/Footer";
 
 function App() {
   useEffect(() => {
@@ -16,6 +17,7 @@ function App() {
   return (
     <Router>
       <Header />
+      <Footer />
     </Router>
   );
 }
