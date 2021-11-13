6810b5f68c214680f8f0e0163038fedea6259fde
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-11-13 21:25:10 +0545

    Loader animation

--- frontend/src/component/home/Home.js
+++ frontend/src/component/home/Home.js
@@ -5,34 +5,46 @@ import "./Home.css";
 import ProductCard from "./ProductCard";
 import { getProduct } from "../../actions/productAction";
 import { useSelector, useDispatch } from "react-redux";
+import Loader from "../layout/loader/Loader";
+import { useAlert } from "react-alert";
 
 const Home = () => {
+  const alert = useAlert();
   const dispatch = useDispatch();
   const { loading, error, products, productCount } = useSelector(
     (state) => state.products
   );
 
   useEffect(() => {
+    if (error) {
+      return alert.error(error);
+    }
     dispatch(getProduct());
-  }, [dispatch]);
+  }, [dispatch, error]);
 
   return (
     <>
-      <MetaData title="Tech-Store" />
-      <div className="banner">
-        <p>Welcome to Tech-Store</p>
-        <h1>FIND AMAZING PRODUCTS BELOW</h1>
-        <a href="#container">
-          <button>
-            Scroll <CgMouse />
-          </button>
-        </a>
-      </div>
-      <h2 className="homeHeading">Featured Products</h2>
-      <div className="container" id="container">
-        {products &&
-          products.map((product) => <ProductCard product={product} />)}
-      </div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <MetaData title="Tech-Store" />
+          <div className="banner">
+            <p>Welcome to Tech-Store</p>
+            <h1>FIND AMAZING PRODUCTS BELOW</h1>
+            <a href="#container">
+              <button>
+                Scroll <CgMouse />
+              </button>
+            </a>
+          </div>
+          <h2 className="homeHeading">Featured Products</h2>
+          <div className="container" id="container">
+            {products &&
+              products.map((product) => <ProductCard product={product} />)}
+          </div>
+        </>
+      )}
     </>
   );
 };
--- frontend/src/component/home/ProductCard.js
+++ frontend/src/component/home/ProductCard.js
@@ -8,7 +8,7 @@ const ProductCard = ({ product }) => {
     color: "rgba(20,20,20,0.1)",
     activeColor: "tomato",
     size: window.innerWidth < 600 ? 20 : 25,
-    value: 2.5,
+    value: product.ratings,
     isHalf: true,
   };
 
@@ -17,9 +17,10 @@ const ProductCard = ({ product }) => {
       <img src={product.images[0].url} alt={product.name} />
       <p>{product.name}</p>
       <div>
-        <ReactStars {...options} /> <span>(360 Reviews)</span>
+        <ReactStars {...options} />{" "}
+        <span>({product.numOfReviews} Reviews)</span>
       </div>
-      <span>{product.price}</span>
+      <span>{`Nrp ${product.price}`}</span>
     </Link>
   );
 };
